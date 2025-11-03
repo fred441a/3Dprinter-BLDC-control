6afd3d4288fde6dce5b7e1b8e15f41a978b53152
Frederik Hansen <frederik@altofte.dk>
2025-11-03 15:34:23 +0100

    better step response code

--- RPI_2040/step_response/step_response.ino
+++ RPI_2040/step_response/step_response.ino
@@ -1,4 +1,4 @@
-const int resolution = 16;
+const int resolution = 64;
 const int encoder = 17;
 
 void setup() {
@@ -6,13 +6,28 @@ void setup() {
   Serial.begin(115200);
 }
 
-void loop() {
+int get_angle() {
+
   int sum = 0;
   int i = 0;
-  while (i < resolution) {
+  while (i < resolution*4) {
     sum += digitalRead(encoder);
     i++;
-    delayMicroseconds(69);
+    delayMicroseconds(17);
   }
-  Serial.println(sum*1000 / 16);
+
+  return (sum * 1000 / (resolution*4));
+}
+
+void loop() {
+  static int angle, angle2 = 0;
+  angle = get_angle();
+  delay(100);
+  angle2 = get_angle();
+  //Serial.printf("angle: %i,angle2: %i,speed: %i \n",angle,angle2,abs(angle2-angle));
+  int speed = abs(angle2-angle);
+  if(angle > angle2){
+    speed = (1000-angle)+angle2;
+  };
+  Serial.println(speed);
 }
