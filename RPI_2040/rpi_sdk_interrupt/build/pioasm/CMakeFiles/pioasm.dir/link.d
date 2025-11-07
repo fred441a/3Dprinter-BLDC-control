1721b94caefb62e4ecb2987f0e9b1d2920d74174
Frederik Hansen <frederik@altofte.dk>
2025-11-07 12:02:22 +0100

    her ditte her er min lorte måle kode

---
+++ RPI_2040/rpi_sdk_interrupt/build/pioasm/CMakeFiles/pioasm.dir/link.d
@@ -0,0 +1,124 @@
+pioasm: \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/crt1.o \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/crti.o \
+  /nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtbegin.o \
+  CMakeFiles/pioasm.dir/main.cpp.o \
+  CMakeFiles/pioasm.dir/pio_assembler.cpp.o \
+  CMakeFiles/pioasm.dir/pio_disassembler.cpp.o \
+  CMakeFiles/pioasm.dir/gen/lexer.cpp.o \
+  CMakeFiles/pioasm.dir/gen/parser.cpp.o \
+  CMakeFiles/pioasm.dir/c_sdk_output.cpp.o \
+  CMakeFiles/pioasm.dir/python_output.cpp.o \
+  CMakeFiles/pioasm.dir/hex_output.cpp.o \
+  CMakeFiles/pioasm.dir/json_output.cpp.o \
+  CMakeFiles/pioasm.dir/ada_output.cpp.o \
+  CMakeFiles/pioasm.dir/go_output.cpp.o \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libstdc++.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so.6 \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libmvec.so.1 \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so.1 \
+  /nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so.6 \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc_nonshared.a \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/ld-linux-x86-64.so.2 \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so.1 \
+  /nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtend.o \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/crtn.o \
+  /nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/ld-linux-x86-64.so.2
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/crt1.o:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/crti.o:
+
+/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtbegin.o:
+
+CMakeFiles/pioasm.dir/main.cpp.o:
+
+CMakeFiles/pioasm.dir/pio_assembler.cpp.o:
+
+CMakeFiles/pioasm.dir/pio_disassembler.cpp.o:
+
+CMakeFiles/pioasm.dir/gen/lexer.cpp.o:
+
+CMakeFiles/pioasm.dir/gen/parser.cpp.o:
+
+CMakeFiles/pioasm.dir/c_sdk_output.cpp.o:
+
+CMakeFiles/pioasm.dir/python_output.cpp.o:
+
+CMakeFiles/pioasm.dir/hex_output.cpp.o:
+
+CMakeFiles/pioasm.dir/json_output.cpp.o:
+
+CMakeFiles/pioasm.dir/ada_output.cpp.o:
+
+CMakeFiles/pioasm.dir/go_output.cpp.o:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libstdc++.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libm.so.6:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libmvec.so.1:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so.1:
+
+/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc.so.6:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/libc_nonshared.a:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/ld-linux-x86-64.so.2:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/41ym1jm1b7j3rhglk82gwg9jml26z1km-gcc-14.3.0-lib/lib/libgcc_s.so.1:
+
+/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/82kmz7r96navanrc2fgckh2bamiqrgsw-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtend.o:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/crtn.o:
+
+/nix/store/776irwlgfb65a782cxmyk61pck460fs9-glibc-2.40-66/lib/ld-linux-x86-64.so.2:
