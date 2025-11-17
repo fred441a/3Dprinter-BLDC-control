01b7eb26ce543627c9479e293f0e5fd22e36e03d
BanDippen <dittetheilgaard@hotmail.com>
2025-11-17 16:00:14 +0100

    antiwindup

--- RPI_2040/rpi_motor_control/main.cpp
+++ RPI_2040/rpi_motor_control/main.cpp
@@ -7,18 +7,20 @@
 
 // const uint gpio = 17;
 const double wanted_ws = 9;
+const float Kaw = 3.2;
 
 int main() {
   stdio_init_all();
   Encoder *encoder = new Encoder(19);
   step_response(encoder);
   Motor *motor = new Motor(16, 0.3599);
-  PID *pid = new PID(0.048, 3.2, 0);
+  PID *pid = new PID(0.048, 3.2, 0, Kaw);
 
   while (true) {
+      printf("BOOBOO");
     float ws = encoder->get_ws();
     float voltage = pid->voltage(encoder->get_ws(), wanted_ws);
-    printf("voltage:%f,WS:%f\n", voltage, ws);
+    printf("with AW: voltage:%f,WS:%f\n", voltage, ws);
     motor->set_voltage(voltage);
     // sleep_ms(1000);
   }
--- RPI_2040/rpi_motor_control/pid.cpp
+++ RPI_2040/rpi_motor_control/pid.cpp
@@ -1,17 +1,33 @@
 
 
-class PID {
+class PID
+{
 private:
-  float Kp, Ki, Kd;
+  float Kp, Ki, Kd, Kaw;
+  float limit = 16;
 
 public:
-  PID(float Kp, float Ki, float Kd) : Kp(Kp), Ki(Ki), Kd(Kd) {}
-  double voltage(double ws_measure, double ws_wanted) {
+  PID(float Kp, float Ki, float Kd, float Kaw) : Kp(Kp), Ki(Ki), Kd(Kd), Kaw(Kaw) {}
+  double voltage(double ws_measure, double ws_wanted)
+  {
     static double integral = 0;
     static double last_e = 0;
+    static float last_pid = 0;
+    static float last_pidPure = 0;
+    static float anti_windup = 0;
+    static float pid = 0;
     double e = ws_wanted - ws_measure;
-    integral += Ki * e;
-    return (Kp * e) + integral + (Kd * (last_e - e));
+
+    anti_windup = (last_pid-last_pidPure) * Kaw;
+    integral += (Ki+anti_windup) * e;
+    pid = (Kp * e) + integral + (Kd * (last_e - e));
+    if (pid > limit)
+    {
+      last_pidPure = pid;
+      pid = limit;
+    }
     last_e = e;
+    last_pid = pid;
+    return pid;
   };
 };
