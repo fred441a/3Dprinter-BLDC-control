ac8ab8a4833cdc5e74b96be80591a30d2fc31565
Frederik Hansen <frederik@altofte.dk>
2025-12-02 11:00:53 +0100

    fixed i2c

--- RPI_2040/rpi_sdk_i2c_measure/main.cpp
+++ RPI_2040/rpi_sdk_i2c_measure/main.cpp
@@ -33,48 +33,27 @@ bool reserved_addr(uint8_t addr) {
 int main() {
   // Enable UART so we can print status output
   stdio_init_all();
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) ||             \
-    !defined(PICO_DEFAULT_I2C_SCL_PIN)
-#warning i2c/bus_scan example requires a board with I2C pins
-  puts("Default I2C pins were not defined");
-#else
   // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a
   // Pico)
   i2c_init(i2c_default, 100 * 1000);
-  i2c_set_baudrate(i2c_default, 50000);
-  gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-  gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+  i2c_set_baudrate(i2c_default, 100 * 1000);
+  gpio_set_function(4, GPIO_FUNC_I2C);
+  gpio_set_function(5, GPIO_FUNC_I2C);
   gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
   gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
   // Make the I2C pins available to picotool
-  bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN,
-                             GPIO_FUNC_I2C));
+  bi_decl(bi_2pins_with_func(4, 5, GPIO_FUNC_I2C));
 
-  printf("\nI2C Bus Scan\n");
-  printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+  for (;;) {
+    printf("Reading \n");
+    uint8_t buff[2];
+    uint8_t data = 0x0E;
+    int ret = 0;
+    ret = i2c_write_blocking(i2c_default, 0x36, &data, 1, true);
+    ret = i2c_read_burst_blocking(i2c_default, 0x36, buff, 2);
 
-  for (int addr = 0; addr < (1 << 7); ++addr) {
-    if (addr % 16 == 0) {
-      printf("%02x ", addr);
-    }
-
-    // Perform a 1-byte dummy read from the probe address. If a slave
-    // acknowledges this address, the function returns the number of bytes
-    // transferred. If the address byte is ignored, the function returns
-    // -1.
-
-    // Skip over any reserved addresses.
-    int ret;
-    uint8_t rxdata;
-    if (reserved_addr(addr))
-      ret = PICO_ERROR_GENERIC;
-    else
-      ret = i2c_read_blocking_until(i2c_default, addr, &rxdata, 1, false, 50);
-
-    printf(ret < 0 ? "." : "@");
-    printf(addr % 16 == 15 ? "\n" : "  ");
+    printf("%u", ((buff[0] << 8) | buff[1]));
   }
-  printf("Done.\n");
+
   return 0;
-#endif
 }
