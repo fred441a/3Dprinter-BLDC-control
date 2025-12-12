fe61adda1e81edbf5915cb20eddf20ba6f103195
Frederik Hansen <frederik@altofte.dk>
2025-12-12 11:27:48 +0100

    merge with dittes

--- RPI_2040/rpi_motor_control/main.cpp
+++ RPI_2040/rpi_motor_control/main.cpp
@@ -1,11 +1,16 @@
+#include "crc.cpp"
 #include "encoder.cpp"
 #include "motor.cpp"
+#include "pico/cyw43_arch.h"
+#include "pico/multicore.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "pid.cpp"
 #include "step_response.cpp"
+#include <mutex>
 
 // const uint gpio = 17;
+std::mutex *ws_lock = new std::mutex;
 float wanted_ws = 12;
 const float T = 0.0105;
 int c = 1;
@@ -14,19 +19,45 @@ float KP = 0.11;
 float KI = 1.42;
 float KD = 0;
 
-int main()
-{
+#pragma pack(0)
+struct Rx_Packet {
+  int16_t ws;
+  uint8_t crc;
+};
+
+void core1_main() {
+  cyw43_arch_init();
+  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+  printf("starting core2");
+
+  uart_init(uart1, 115200);
+  gpio_set_function(8, UART_FUNCSEL_NUM(uart1, 0));
+  gpio_set_function(9, UART_FUNCSEL_NUM(uart1, 1));
+
+  // struct Rx_Packet send;
+  //  send.ws = 115;
+  //  send.crc = CRC8((const char *)&send, 2);
+  char recieve[3];
+
+  while (true) {
+    if (uart_is_readable(uart1)) {
+      uart_read_blocking(uart1, (uint8_t *)&recieve, 3);
+      printf("%i\n", (int)((recieve[2] << 8) | recieve[1]));
+    }
+  }
+}
+
+int main() {
   stdio_init_all();
+  multicore_launch_core1(core1_main);
   Encoder *encoder = new Encoder(5, 4);
   Motor *motor = new Motor(16, 0.3599);
   PID *pid = new PID(KP, KI, KD);
   sleep_ms(5000);
-  printf("begin");
-  while (true)
-  {
+  while (true) {
     float ws;
     static float voltage_pid = 0;
-    printf("%lld,%f,%f \n", get_absolute_time(), ws, voltage_pid);
+    // printf("%lld,%f,%f \n", get_absolute_time(), ws, voltage_pid);
 
     ws = encoder->get_ws();
     voltage_pid = pid->voltageDis(ws, wanted_ws, T);
--- RPI_2040/rpi_motor_control/pid.cpp
+++ RPI_2040/rpi_motor_control/pid.cpp
@@ -1,7 +1,7 @@
 
 
-class PID
-{
+#include <mutex>
+class PID {
 private:
   float Kp, Ki, Kd;
   float limit = 6;
@@ -9,15 +9,17 @@ private:
 public:
   PID(float Kp, float Ki, float Kd) : Kp(Kp), Ki(Ki), Kd(Kd) {}
 
-  float voltageDis(float ws_measure, float ws_wanted, float Ts)
-  {
+  float voltageDis(float ws_measure, float ws_wanted, std::mutex ws_lock,
+                   float Ts) {
     static float last_e = 0;
     static float e_last = 0;
     static float u_last = 0;
+	ws_lock.lock();
 
     float e = ws_wanted - ws_measure;
 
-    float u = Kp*e - Kp*e_last+Ki*e_last*Ts+u_last; //Kp*e - Kp*e_last + e*Ki*T + v_last;
+    float u = Kp * e - Kp * e_last + Ki * e_last * Ts +
+              u_last; // Kp*e - Kp*e_last + e*Ki*T + v_last;
     u_last = u;
     e_last = e;
 
